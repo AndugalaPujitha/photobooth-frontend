7330b4698642de9039cd6f8bd1577d69747a2ae9
Pujitha <22b01a4502@svecw.edu.in>
2025-04-30 13:22:51 +0530

    Update commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import PhotoBooth from "./PhotoBooth";
+import Footer from "./Footer";
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/booth" element={<PhotoBooth />} />
-      </Routes>
+      <div className="app-wrapper">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/booth" element={<PhotoBooth />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer className="footer">
+        <p>Made with ❤️ by Pujitha</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
